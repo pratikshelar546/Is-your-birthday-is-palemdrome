d938737464764b5585039683ce47785699bdc3f7
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2022-07-11 17:21:20 +0530

    js done

--- logic.js
+++ logic.js
@@ -50,17 +50,81 @@ function checkAllDateFormat(date){
     }
     return flag;
 }
+function isLeapYear(year){
+  if(year % 400 === 0)
+    return true;
 
-function nextDay(){
-    const day = day+1;
+    if(year % 100 === 0)
+    return false;
+
+    if(year % 4 === 0)
+    return true;
+
+    return false
 }
 
+function nextDay(date){
+    var day = date.day+1;
+    var month = date.month;
+    var year = date.year;
+    
+    var dayInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    
+    if(month ===2){
+      if(isLeapYear(year)){
+        if(day > 29){
+          day =1;
+          month=3;
+        }
+      }
+      else{
+        if(day>28){
+          day =1;
+          month=3;
+        }
+      }
+    }
+    else{
+      if(day > dayInMonth[month - 1]){
+         day = 1;
+        month++;
+      }
+    }
+    if(month > 12){
+      month =1;
+      year++;
+    }
+    return {
+      day :day,
+      month: month,
+      year:year
+    }
+}
+function getNextPalemdromeDate(date){
+  var ctr = 0;
+var nextDate= nextDay(date);
+while(1){
+  ctr++;
+
+  var listResult = checkAllDateFormat(nextDate);
+
+    if(listResult){
+
+      console.log(ctr, nextDate)
+    }
+    nextDate = nextDay(nextDate);
+  }
+  // return true
+}
+
+
+
 var date = {
-    day: 2,
-    month: 11,
+    day: 3,
+    month: 12,
     year:2020
 }
 
-console.log(checkAllDateFormat(date));
+console.log(getNextPalemdromeDate(date));
 //  console.log(checkStrIsPalemdrome("oppo"));
 //  console.log(checkStrIsPalemdrome("nayan"));
